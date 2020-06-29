3ee3b51223431c2669aa0bad8662fe494555fae6
Donald Silveira <donaldsilveira@gmail.com>
2020-06-29 16:39:13 -0300

    feat(navigation): Created Login and CareersEdit stack

--- src/navigation/app.navigator.tsx
+++ src/navigation/app.navigator.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import MainNavigator from './main.navigator';
 
-const AppNavigator:React.FC = () => (
-  <NavigationContainer>
-    <MainNavigator initialRouteName="CareersList" />
-  </NavigationContainer>
-);
+import MainNavigator from './main.navigator';
+import { useStores } from '../models';
+
+const AppNavigator:React.FC = () => {
+  const { userStore } = useStores();
+
+  return (
+    <NavigationContainer>
+      <MainNavigator initialRouteName={userStore.username ? 'CareersList' : 'Login'} />
+    </NavigationContainer>
+  );
+};
 
 export default AppNavigator;
--- src/navigation/main.navigator.tsx
+++ src/navigation/main.navigator.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { CareerSnapshot } from '../models/career';
+import Login from '../screens/login';
 import CareersList from '../screens/careers-list';
+import CareersEdit from '../screens/careers-edit';
 
-const Stack = createStackNavigator();
+type RootStackParamsList = {
+  Login: undefined,
+  CareersList: undefined,
+  CareersEdit: { career: CareerSnapshot },
+}
+
+const Stack = createStackNavigator<RootStackParamsList>();
 
 type MainNavigatorProps = {
-  initialRouteName: string,
+  initialRouteName?: 'Login' | 'CareersList' | 'CareersEdit' | undefined,
 };
 
 const MainNavigator:React.FC<MainNavigatorProps> = ({ initialRouteName }) => (
   <Stack.Navigator headerMode="none" initialRouteName={initialRouteName}>
+    <Stack.Screen name="Login" component={Login} />
     <Stack.Screen name="CareersList" component={CareersList} />
+    <Stack.Screen name="CareersEdit" component={CareersEdit} />
   </Stack.Navigator>
 );
 
