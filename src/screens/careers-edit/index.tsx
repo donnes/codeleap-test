4d1cc0877d4c66bb73b4a7e1777b5887da84f2ed
Donald Silveira <donaldsilveira@gmail.com>
2020-06-29 23:43:09 -0300

    feat(careers-edit): Implemented career update at screen

--- src/screens/careers-edit/index.tsx
+++ src/screens/careers-edit/index.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import { useRoute, RouteProp, ParamListBase } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { useObserver } from 'mobx-react-lite';
 import CareerForm from '../../components/career-form';
 
 import { Container } from './styles';
-// import { useStores } from '../../models';
-
-type ProfileScreenRouteProp = RouteProp<ParamListBase, 'CareersEdit'>;
+import { useStores } from '../../models';
 
 const CareersEdit:React.FC = () => {
-  const route = useRoute<ProfileScreenRouteProp>();
-  // const { careerStore } = useStores();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { careerStore } = useStores();
 
   const career = route.params?.career;
 
-  return (
+  const handleOnSave = async (title: string, content: string) => {
+    await careerStore.update(career.id, { title, content });
+    navigation.goBack();
+  };
+
+  return useObserver(() => (
     <Container>
       <CareerForm
         title="Edit item"
-        onSave={() => null}
+        onSave={handleOnSave}
         initialValues={{
           title: career.title,
           content: career.content,
         }}
+        submitting={careerStore.submitting}
+        cancelable
       />
     </Container>
-  );
+  ));
 };
 
 export default CareersEdit;
